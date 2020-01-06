9ae5c1bdff971e322e5c500ee5b79af2714b4aca
kqt <123456@qq.com>
2020-01-06 13:48:01 +0800

    第一题

---
+++ app/src/main/java/com/lenovo/manufacture/ReUse/MyRe.java
@@ -0,0 +1,47 @@
+package com.lenovo.manufacture.ReUse;
+
+import org.json.JSONObject;
+
+import java.util.Map;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public class MyRe {
+    static JSONObject jsonObject;
+    public interface API {
+        @FormUrlEncoded
+        @POST(".")
+        Call<ResponseBody> getpost(@FieldMap Map<String, String> a);
+    }
+    public static JSONObject re(Map<String,String> a,String b) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.112:8085"+b+"/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        API api = retrofit.create(API.class);
+        Call<ResponseBody> task = api.getpost(a);
+        task.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    String a=response.body().string();
+                    jsonObject=new JSONObject(a);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {}
+        });
+
+        return jsonObject;
+    }
+}
--- app/src/main/java/com/lenovo/manufacture/TestActivity.java
+++ app/src/main/java/com/lenovo/manufacture/TestActivity.java
@@ -1,6 +1,22 @@
 package com.lenovo.manufacture;
 
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.lenovo.manufacture.ReUse.MyRe;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,11 +28,102 @@ import androidx.appcompat.app.AppCompatActivity;
  * email：
  * description：
  */
-public class TestActivity extends AppCompatActivity {
+public class TestActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView mI1;
+    /**
+     * 开启
+     */
+    private Button mB1;
+    /**
+     * 关闭
+     */
+    private Button mB2;
+    private ImageView mBi;
+    /**
+     *
+     */
+    private TextView mBt;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        initView();
+
+        HashMap<String,String> m = new HashMap<>();
+
+        m.put("id","1");
+        MyRe.re(m, "/dataInterface/UserWorkEnvironmental/getInfo");
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                JSONObject j = MyRe.re(m, "/dataInterface/UserWorkEnvironmental/getInfo");
+                if (j !=null) {
+                    try {
+                        if (j.getString("message").equals("SUCCESS")) {
+                            JSONArray data = j.getJSONArray("data");
+                            JSONObject js = data.getJSONObject(0);
+                            final String lightSwitch = js.getString("lightSwitch");
+                            if (lightSwitch.equals("1")) {
+                                mI1.setBackgroundResource(R.drawable.gongchang_01);
+                            } else if (lightSwitch.equals("0")) {
+                                mI1.setBackgroundResource(R.drawable.gongchang02);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        },0,1000);
 
     }
+
+
+    private void initView() {
+        mI1 = (ImageView) findViewById(R.id.I1);
+        mB1 = (Button) findViewById(R.id.b1);
+        mB1.setOnClickListener(this);
+        mB2 = (Button) findViewById(R.id.b2);
+        mB2.setOnClickListener(this);
+        mBi = (ImageView) findViewById(R.id.bi);
+        mBi.setOnClickListener(this);
+        mBt = (TextView) findViewById(R.id.bt);
+        mBt.setText("灯光控制");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.b1:
+                mI1.setBackgroundResource(R.drawable.gongchang_01);
+                openLight();
+                break;
+            case R.id.b2:
+                mI1.setBackgroundResource(R.drawable.gongchang02);
+                closeLight();
+                break;
+            case R.id.bi:
+                finish();
+                break;
+        }
+    }
+
+    private void openLight() {
+
+        HashMap<String,String>  m = new HashMap<>();
+        m.put("id","1");
+        m.put("lightSwitch","1");
+        MyRe.re(m,"/dataInterface/UserWorkEnvironmental/updateLightSwitch");
+    }
+
+    private void closeLight() {
+        HashMap<String,String>  m = new HashMap<>();
+        m.put("id","1");
+        m.put("lightSwitch","0");
+        MyRe.re(m,"/dataInterface/UserWorkEnvironmental/updateLightSwitch");
+    }
+
 }
