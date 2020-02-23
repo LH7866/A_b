821caf655db98d221dc994425d26e50bf278c082
zhy <zhy@123.com>
2020-02-23 21:14:34 +0800

    Revert "14"
    This reverts commit 7167c9aa

---
+++ app/src/main/java/com/lenovo/manufacture/b/fragment/Temp.java
@@ -0,0 +1,127 @@
+package com.lenovo.manufacture.b.fragment;
+
+import java.util.List;
+
+public class Temp {
+    /*
+    *
+    *
+    * */
+
+
+
+
+
+            /**
+             * id : 1
+             * day : 461
+             * lightSwitch : 1
+             * acOnOff : 1
+             * beam : 245
+             * workshopTemp : 17℃
+             * outTemp : 30℃
+             * power : 100
+             * powerConsume : 78
+             * time : 10:15
+             */
+
+            private String id;
+            private String day;
+            private int lightSwitch;
+            private String acOnOff;
+            private String beam;
+            private String workshopTemp;
+            private String outTemp;
+            private String power;
+            private String powerConsume;
+            private String time;
+
+            public String getId() {
+                return id;
+            }
+
+            public void setId(String id) {
+                this.id = id;
+            }
+
+            public String getDay() {
+                return day;
+            }
+
+            public void setDay(String day) {
+                this.day = day;
+            }
+
+            public int getLightSwitch() {
+                return lightSwitch;
+            }
+
+            public void setLightSwitch(int lightSwitch) {
+                this.lightSwitch = lightSwitch;
+            }
+
+            public String getAcOnOff() {
+                return acOnOff;
+            }
+
+            public void setAcOnOff(String acOnOff) {
+                this.acOnOff = acOnOff;
+            }
+
+            public String getBeam() {
+                return beam;
+            }
+
+            public void setBeam(String beam) {
+                this.beam = beam;
+            }
+
+            public String getWorkshopTemp() {
+                return workshopTemp;
+            }
+
+            public void setWorkshopTemp(String workshopTemp) {
+                this.workshopTemp = workshopTemp;
+            }
+
+            public String getOutTemp() {
+                return outTemp;
+            }
+
+            public void setOutTemp(String outTemp) {
+                this.outTemp = outTemp;
+            }
+
+            public String getPower() {
+                return power;
+            }
+
+            public void setPower(String power) {
+                this.power = power;
+            }
+
+            public String getPowerConsume() {
+                return powerConsume;
+            }
+
+            public void setPowerConsume(String powerConsume) {
+                this.powerConsume = powerConsume;
+            }
+
+            public String getTime() {
+                return time;
+            }
+
+            public void setTime(String time) {
+                this.time = time;
+            }
+
+    public void saveAsynns() {
+
+
+
+
+    }
+}
+
+
