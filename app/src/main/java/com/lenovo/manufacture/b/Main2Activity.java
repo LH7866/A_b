8387f06a732fbc329e485bb2418abaf8b27baae9
zhy <zhy@123.com>
2020-02-24 17:35:45 +0800

    Revert "15"
    This reverts commit 7167c9aa

--- app/src/main/java/com/lenovo/manufacture/b/Main2Activity.java
+++ app/src/main/java/com/lenovo/manufacture/b/Main2Activity.java
@@ -27,6 +27,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -50,6 +51,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     private TextView mT3;
     private LineChart mLc;
     private Timer t;
+    List<Temp> temps=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,107 +60,122 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         LitePal.initialize(Main2Activity.this);
 
         initView();
-            mpch();
-            a();
-    }
-
-    private void a() {
         HashMap<String, String> m = new HashMap<>();
         m.put("id", "1");
         t = new Timer();
         t.schedule(new TimerTask() {
+
+
+
             @Override
             public void run() {
                 JSONObject j = MyRe.re(m, "/dataInterface/UserWorkEnvironmental/getInfo");
                 try {
-                    Temp temp = new Temp();
                     if (j != null) {
                         if (j.getString("message").equals("SUCCESS")) {
                             JSONArray data = j.getJSONArray("data");
                             JSONObject js = data.getJSONObject(0);
-                            temp.setWorkshopTemp(js.getString("workshopTemp"));
-                            temp.setOutTemp(js.getString("outTemp"));
                             String workshopTemp = js.getString("workshopTemp");
                             String outTemp = js.getString("outTemp");
-                            Log.i("666666",outTemp+"");
-                            mT2.setText(outTemp);
-                            mT3.setText(workshopTemp);
-                        }
+                            Log.i("666666", outTemp + "");
+//                            mT2.setText(outTemp);
+//                            mT3.setText(workshopTemp);
 
+                            for(int i=0;i<temps.size();i++){
+                                Temp temp=new Temp();
+                                temp.setOutTemp(js.getString("workshopTemp"));
+                                temp.setWorkshopTemp(js.getString("outTemp"));
+                                temps.add(temp);
+                            }
+                            mLc.setNoDataText("nodata");
+                            Description description = new Description();
+                            description.setEnabled(false);
+                            mLc.setDescription(description);
+                            mLc.setBackgroundColor(Color.WHITE);
+
+
+                            List<Entry> list = new ArrayList<>();
+                            List<Entry> list1 = new ArrayList<>();
+
+                            //data
+                            List<Temp> temps = LitePal.order("id desc").limit(12).find(Temp.class);
+                            Log.i("99999999",temps+"");
+                            int i = 0;
+                            for (Temp temp : temps) {
+                                String s = temp.getWorkshopTemp();
+                                s = s.substring(0, s.length() - 1);
+                                String ss = temp.getOutTemp();
+                                ss = ss.substring(0, ss.length() - 1);
+                                list1.add(new Entry(i, Integer.parseInt(s)));
+                                list.add(new Entry(i++, Integer.parseInt(ss)));
+                            }
+
+                            LineDataSet dataSet1 = new LineDataSet(list, "");
+                            dataSet1.setDrawCircles(false);
+                            dataSet1.setColor(Color.parseColor("#FF5B5B"));
+                            dataSet1.setDrawValues(false);
+
+
+                            LineDataSet dataSet2 = new LineDataSet(list1, "");
+                            dataSet2.setDrawValues(false);
+                            dataSet2.setColor(Color.parseColor("#82AAFF"));
+                            dataSet2.setDrawValues(false);
+
+
+                            LineData data1 = new LineData();
+                            data1.addDataSet(dataSet1);
+                            data1.addDataSet(dataSet2);
+
+
+                            XAxis axis = mLc.getXAxis();
+                            axis.setPosition(XAxis.XAxisPosition.BOTTOM);
+                            axis.setLabelCount(12);
+                            String[] time = new String[]{"00:00", "02:00", "04:00", "06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00", "24:00"};
+                            axis.setValueFormatter(new ValueFormatter() {
+                                @Override
+                                public String getFormattedValue(float value, AxisBase axis) {
+                                    return time[(int) value];
+                                }
+                            });
+
+                            //关闭右
+                            mLc.getAxisRight().setEnabled(false);
+                            YAxis yAxis = mLc.getAxisLeft();
+                            yAxis.setLabelCount(10);
+                            yAxis.setValueFormatter(new ValueFormatter() {
+                                @Override
+                                public String getFormattedValue(float value, AxisBase axis) {
+                                    return "";
+
+                                }
+                            });
+
+                            mLc.getLegend().setEnabled(false);
+                            mLc.setData(data1);
+                        }
                     }
+
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
+
+
+
             }
         }, 0, 5000);
+
+
+
     }
 
+    private void a() {
+
+    }
+
+
+
     private void mpch() {
-        mLc.setNoDataText("nodata");
-        Description description = new Description();
-        description.setEnabled(false);
-        mLc.setDescription(description);
-        mLc.setBackgroundColor(Color.WHITE);
 
-
-        List<Entry> list = new ArrayList<>();
-        List<Entry> list1 = new ArrayList<>();
-
-        //data
-        List<Temp> temps = LitePal.order("id desc").limit(12).find(Temp.class);
-
-        int i = 0;
-        for (Temp temp : temps) {
-            String s = temp.getWorkshopTemp();
-            s = s.substring(0, s.length() - 1);
-            String ss = temp.getOutTemp();
-            ss = ss.substring(0, ss.length() - 1);
-            list1.add(new Entry(i, Integer.parseInt(s)));
-            list.add(new Entry(i++, Integer.parseInt(ss)));
-        }
-
-        LineDataSet dataSet1 = new LineDataSet(list, "");
-        dataSet1.setDrawCircles(false);
-        dataSet1.setColor(Color.parseColor("#FF5B5B"));
-        dataSet1.setDrawValues(false);
-
-
-        LineDataSet dataSet2 = new LineDataSet(list1, "");
-        dataSet2.setDrawValues(false);
-        dataSet2.setColor(Color.parseColor("#82AAFF"));
-        dataSet2.setDrawValues(false);
-
-
-        LineData data = new LineData();
-        data.addDataSet(dataSet1);
-        data.addDataSet(dataSet2);
-
-
-        XAxis axis = mLc.getXAxis();
-        axis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        axis.setLabelCount(12);
-        String[] time = new String[]{"00:00", "02:00", "04:00", "06:00", "08:00", "10:00", "12:00", "14:00", "16:00", "18:00", "20:00", "22:00", "24:00"};
-        axis.setValueFormatter(new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return time[(int) value];
-            }
-        });
-
-        //关闭右
-        mLc.getAxisRight().setEnabled(false);
-        YAxis yAxis = mLc.getAxisLeft();
-        yAxis.setLabelCount(10);
-        yAxis.setValueFormatter(new ValueFormatter() {
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-                return "";
-
-            }
-        });
-
-        mLc.getLegend().setEnabled(false);
-        mLc.setData(data);
 
 
 //
--- app/src/main/java/com/lenovo/manufacture/b/entity/Temp.java
+++ app/src/main/java/com/lenovo/manufacture/b/entity/Temp.java
@@ -1,145 +1,38 @@
 package com.lenovo.manufacture.b.entity;
 
+import org.litepal.LitePal;
+import org.litepal.crud.LitePalSupport;
+
 import java.util.List;
 
-public class Temp {
-    /**
-     * status : 200
-     * message : SUCCESS
-     * data : [{"id":"1","day":"461","lightSwitch":1,"acOnOff":"1","beam":"245","workshopTemp":"17℃","outTemp":"30℃","power":"100","powerConsume":"78","time":"10:15"}]
-     */
+public class Temp extends LitePalSupport {
 
-    private int status;
-    private String message;
-    private List<DataBean> data;
-
-    public int getStatus() {
-        return status;
+    public String getWorkshopTemp() {
+        return workshopTemp;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    @Override
+    public String toString() {
+        return "Temp{" +
+                "workshopTemp='" + workshopTemp + '\'' +
+                ", outTemp='" + outTemp + '\'' +
+                '}';
     }
 
-    public String getMessage() {
-        return message;
+    public void setWorkshopTemp(String workshopTemp) {
+        this.workshopTemp = workshopTemp;
     }
 
-    public void setMessage(String message) {
-        this.message = message;
+    public String getOutTemp() {
+        return outTemp;
     }
 
-    public List<DataBean> getData() {
-        return data;
+    public void setOutTemp(String outTemp) {
+        this.outTemp = outTemp;
     }
 
-    public void setData(List<DataBean> data) {
-        this.data = data;
-    }
-
-    public static class DataBean {
-        /**
-         * id : 1
-         * day : 461
-         * lightSwitch : 1
-         * acOnOff : 1
-         * beam : 245
-         * workshopTemp : 17℃
-         * outTemp : 30℃
-         * power : 100
-         * powerConsume : 78
-         * time : 10:15
-         */
-
-        private String id;
-        private String day;
-        private int lightSwitch;
-        private String acOnOff;
-        private String beam;
-        private String workshopTemp;
+    private String workshopTemp;
         private String outTemp;
-        private String power;
-        private String powerConsume;
-        private String time;
 
-        public String getId() {
-            return id;
-        }
-
-        public void setId(String id) {
-            this.id = id;
-        }
-
-        public String getDay() {
-            return day;
-        }
-
-        public void setDay(String day) {
-            this.day = day;
-        }
-
-        public int getLightSwitch() {
-            return lightSwitch;
-        }
-
-        public void setLightSwitch(int lightSwitch) {
-            this.lightSwitch = lightSwitch;
-        }
-
-        public String getAcOnOff() {
-            return acOnOff;
-        }
-
-        public void setAcOnOff(String acOnOff) {
-            this.acOnOff = acOnOff;
-        }
-
-        public String getBeam() {
-            return beam;
-        }
-
-        public void setBeam(String beam) {
-            this.beam = beam;
-        }
-
-        public String getWorkshopTemp() {
-            return workshopTemp;
-        }
-
-        public void setWorkshopTemp(String workshopTemp) {
-            this.workshopTemp = workshopTemp;
-        }
-
-        public String getOutTemp() {
-            return outTemp;
-        }
-
-        public void setOutTemp(String outTemp) {
-            this.outTemp = outTemp;
-        }
-
-        public String getPower() {
-            return power;
-        }
-
-        public void setPower(String power) {
-            this.power = power;
-        }
-
-        public String getPowerConsume() {
-            return powerConsume;
-        }
-
-        public void setPowerConsume(String powerConsume) {
-            this.powerConsume = powerConsume;
-        }
-
-        public String getTime() {
-            return time;
-        }
-
-        public void setTime(String time) {
-            this.time = time;
-        }
     }
-}
+
